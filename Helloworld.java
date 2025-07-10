2e172ca24e880941b8ea9acf65d1f9a1d5ad6b15
swetha635 <swetharamaadhi@gmail.com>
2025-07-10 11:27:32 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,6 @@ public class Helloworld
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("World!");
-		
+		System.out.println("welcome to devoopps");
 	}
 }
